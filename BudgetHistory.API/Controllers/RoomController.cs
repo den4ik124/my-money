252673c1e8b28ae7efdfbad0dc2ce89b7438138b
Denys Boryhin <denys.boryhin@nixsolutions.com>
2022-08-29 20:20:55 +0300

    Controller has been implemented

---
+++ BudgetHistory.API/Controllers/RoomController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notebook.API.Controllers;
+using System;
+using System.Threading.Tasks;
+
+namespace BudgetHistory.API.Controllers
+{
+    public class RoomController : BaseApiController
+    {
+        [HttpGet()]
+        public async Task<IActionResult> GetRoomsList()
+        {
+            return new OkObjectResult("Rooms list here");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRoomById(Guid id)
+        {
+            return new OkObjectResult(id);
+        }
+    }
+}
